47a28f134f67b25bdba2c9cfba6e67e86212a812
Dmitriy877 <dmitriybukin05@gmail.com>
2025-11-13 21:22:28 +0300

    Add echobot for vk

---
+++ vk_bot.py
@@ -0,0 +1,29 @@
+import random
+
+import vk_api as vk
+from vk_api.longpoll import VkLongPoll, VkEventType
+from environs import env
+
+
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
+
+
+def main():
+    env.read_env()
+    vk_group_token = env.str('VK_API_KEY')
+
+    vk_session = vk.VkApi(token=vk_group_token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
+
+
+if __name__ == '__main__':
+    main()
