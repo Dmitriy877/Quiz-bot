7b034de17474f9b8d59bed81cb694b23fe05c7ab
Dmitriy877 <dmitriybukin05@gmail.com>
2025-11-17 21:17:16 +0300

    Update varible name in collect_quiz in file function
    Update varible name in collect_quiz in file function

--- collect_quiz_in_file.py
+++ collect_quiz_in_file.py
@@ -7,16 +7,16 @@ def collect_quiz_in_file(filefolder: str) -> None:
     collected_quiz = dict()
     for file in files_in_filefolder:
         with open(PurePath(filefolder, file), 'r', encoding='KOI8-R') as file:
-            questions = file.read()
-            result = str(questions).split("\n\n\n")
-        for s in result:
-            if 'Вопрос' in s:
+            text = file.read()
+            text_blocks = str(text).split("\n\n\n")
+        for text_block in text_blocks:
+            if 'Вопрос' in text_block:
                 question = ''
                 answer = ''
-                for d in s.split("\n\n"):
-                    if 'Вопрос' in d:
-                        question = d.split(':')[1]
-                    if 'Ответ' in d:
-                        answer = d.split(':')[1]
+                for text in text_block.split("\n\n"):
+                    if 'Вопрос' in text:
+                        question = text.split(':')[1]
+                    if 'Ответ' in text:
+                        answer = text.split(':')[1]
                     collected_quiz[question] = answer
     return collected_quiz
